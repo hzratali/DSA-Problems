df615f907ad80aa2e6c041b1ffc6f1183aa0785f
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-05-15 00:17:50 +0530

    Added solution - LeetHub

---
+++ Maximum subset sum - GFG/maximum-subset-sum.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class IntArray
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    long long findMaxSubsetSum(int N, vector<int> &A) {
+        // code here
+        int take = A[0];
+        int nTake = 0;
+        for(int i=1; i<N; i++){
+            int newTake = max(take+A[i], nTake+A[i]);
+            int newNtake = take;
+            take = newTake;
+            nTake = newNtake;
+        }
+        return max(take, nTake);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        IntArray::input(A,N);
+        
+        Solution obj;
+        long long res = obj.findMaxSubsetSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
