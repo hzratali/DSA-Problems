7351160e1e3dff214d9ec7dce3508188bbdfbc06
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-06-18 17:15:08 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Ticket Counter/ticket-counter.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        // code here
+        int l = 1, r = N, k = K;
+        while(r - l + 1 > 2*k){
+            l += k;
+            r -= k;
+        }
+        if(r - l + 1 > k){
+            l += k;
+            return l;
+        }
+        return r;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
