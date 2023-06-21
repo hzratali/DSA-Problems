4eb939c2991ec27b9dd8f66fce933c6e4ac5997e
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-06-21 23:32:55 +0530

    Added solution - LeetHub

---
+++ Reverse Coding - GFG/reverse-coding.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int mod = 1000000007;
+    int sumOfNaturals(int n) {
+        // code here
+        return ((n*1LL*(n+1)) / 2) % mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
