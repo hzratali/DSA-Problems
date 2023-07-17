c9b16877be4a78906955276f4f39894da359da42
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-07-17 23:53:26 +0530

    Time: 42 ms (49.25%), Space: 71.1 MB (93.10%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -11,33 +11,37 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<int> s1, s2;
-        while(l1 != NULL){
-            s1.push(l1->val);
-            l1 = l1->next;
-        }
-        while(l2 != NULL){
-            s2.push(l2->val);
-            l2 = l2->next;
-        }
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
         int sum = 0, carry = 0;
-        ListNode* ans  = new ListNode();
-        while(!s1.empty() || !s2.empty()){
-            if(!s1.empty()){
-                sum += s1.top();
-                s1.pop();
+        ListNode* ans = new ListNode();
+        while(l1 || l2){
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(!s2.empty()){
-                sum += s2.top();
-                s2.pop();
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
             }
             ans->val = sum%10;
-            carry = sum / 10;
+            carry = sum/10;
             ListNode* newNode = new ListNode(carry);
             newNode->next = ans;
             ans = newNode;
             sum = carry;
         }
-        return carry==0 ? ans->next : ans;
+        return carry == 0 ? ans->next : ans;
+    }
+    ListNode* reverseList(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* temp;
+        while(head){
+            temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
     }
 };
