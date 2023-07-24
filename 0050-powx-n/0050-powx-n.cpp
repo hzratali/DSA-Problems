ef844e33e8834e101a4d59b5c88081ed4dfa9534
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-07-24 23:42:08 +0530

    Time: 4 ms (40.18%), Space: 6 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        return binaryExp(x, (long long)n);
+    }
+    double binaryExp(double x, long long n){
+        if(n == 0) return 1;
+        if(n<0) return 1.0/binaryExp(x, -1*n);
+        if(n%2) return x * binaryExp(x*x, (n-1)/2);
+        else return binaryExp(x*x, n/2);
     }
 };
