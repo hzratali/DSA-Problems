013dd634cd2950c54629e10842662476e5fdb5af
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-07-26 00:09:50 +0530

    Time: 606 ms (35.92%), Space: 30.2 MB (32.70%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,7 +1,5 @@
-class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int i = 0;
-        while(arr[i] < arr[i+1]) i++;
-        return i;
-    }
-}
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        i = 0
+        while arr[i] < arr[i+1]: i += 1
+        return i
