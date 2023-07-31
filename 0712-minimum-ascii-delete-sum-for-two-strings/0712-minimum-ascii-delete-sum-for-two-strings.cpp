20e764e2a3651a1ab43e85cc83dca1fc6450be63
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-08-01 00:32:33 +0530

    Time: 48 ms (65.73%), Space: 15.3 MB (41.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
         vector<vector<int>> dp(s1.size(),vector<int>(s2.size(),-1));
-        return computeCost(s1, s2, s1.size()-1, s2.size()-1, dp);
+        return computeCost(s1, s2, 0, 0, dp);
     }
     int computeCost(string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
-        if(i < 0){
+        if(i==s1.size() && j==s2.size()) return 0;
+        if(i == s1.size()){
             int deleteCost = 0;
-            for(int p=0; p<=j; p++) deleteCost += s2[p];
+            for(int p=j; p<s2.size(); p++) deleteCost += s2[p];
             return deleteCost;
         }
-        if(j < 0){
+        if(j == s2.size()){
             int deleteCost = 0;
-            for(int p=0; p<=i; p++) deleteCost += s1[p];
+            for(int p=i; p<s1.size(); p++) deleteCost += s1[p];
             return deleteCost;
         }
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i] == s2[j]) return dp[i][j] = computeCost(s1, s2, i-1, j-1, dp);
+        int ans = INT_MAX;
+        if(s1[i] == s2[j]) ans = computeCost(s1, s2, i+1, j+1, dp);
         else{
-            return dp[i][j] = min(s1[i] + computeCost(s1, s2, i-1, j, dp),
-                       min(s2[j] + computeCost(s1, s2, i, j-1, dp),
-                           s1[i] + s2[j] + computeCost(s1, s2, i-1, j-1, dp)));
+            ans = min(ans, s1[i] + computeCost(s1, s2, i+1, j, dp));
+            ans = min(ans, s2[j] + computeCost(s1, s2, i, j+ 1, dp));
         }
+        return dp[i][j] = ans;
     }
 };
