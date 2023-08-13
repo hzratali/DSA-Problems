5134a88463aed70e6f2f495320d0172d2ef0608f
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-08-14 00:57:11 +0530

    Time: 129 ms (88.31%), Space: 84.9 MB (83.69%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     bool validPartition(vector<int>& nums) {
-        map<int, bool> dp;
-        dp[-1] = true;
-        return f(nums.size()-1, nums, dp);
-    }
-    bool f(int i, vector<int>&nums, map<int, bool> &dp){
-        if(dp.find(i) != dp.end()) return dp[i];
-        bool ans = false;
-        if(i>0 && nums[i]==nums[i-1]) ans |= f(i-2, nums, dp);
-        if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2]){
-            ans |= f(i-3, nums, dp);
+        int n = nums.size();
+        vector<char> dp(n+1, 0);
+        dp[0] = 1;
+        for(int i=0; i<n; i++){
+            int ind = i+1;
+            if(i>0 && nums[i]==nums[i-1]) dp[ind] |= dp[ind-2];
+            if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2]) dp[ind] |= dp[ind-3];
+            if(i>1 && nums[i]==nums[i-1]+1 && nums[i-1]==nums[i-2]+1) dp[ind] |= dp[ind-3];
         }
-        if(i>1 && nums[i]==nums[i-1]+1 && nums[i-1]==nums[i-2]+1){
-            ans |= f(i-3, nums, dp);
-        }
-        dp[i] = ans;
-        return ans;
+        return dp[n];
     }
 };
