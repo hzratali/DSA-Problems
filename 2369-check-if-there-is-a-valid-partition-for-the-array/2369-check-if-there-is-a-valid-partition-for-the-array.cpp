53a383ed88316858f10d4ea09c7947b3810ee257
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-08-14 01:02:42 +0530

    Time: 135 ms (80.61%), Space: 83.7 MB (99.69%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size();
-        vector<char> dp(n+1, 0);
-        dp[0] = 1;
+        vector<bool> dp(3);
+        dp[0] = true;
         for(int i=0; i<n; i++){
             int ind = i+1;
-            if(i>0 && nums[i]==nums[i-1]) dp[ind] |= dp[ind-2];
-            if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2]) dp[ind] |= dp[ind-3];
-            if(i>1 && nums[i]==nums[i-1]+1 && nums[i-1]==nums[i-2]+1) dp[ind] |= dp[ind-3];
+            bool ans = false;
+            if(i>0 && nums[i]==nums[i-1]) ans |= dp[(ind-2)%3];
+            if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2]) ans |= dp[(ind-3)%3];
+            if(i>1 && nums[i]==nums[i-1]+1 && nums[i-1]==nums[i-2]+1) ans |= dp[(ind-3)%3];
+            dp[ind%3] = ans;
         }
-        return dp[n];
+        return dp[n%3];
     }
 };
