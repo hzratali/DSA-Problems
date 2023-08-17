bb6200c3c9101922f7c5636b7a4e60052f0124c8
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-08-17 12:57:35 +0530

    Time: 13 ms (82.79%), Space: 45.3 MB (85.31%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,32 +1,31 @@
 class Solution {
-public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, -1));
-        queue<pair<int, int>> q;
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[][] ans = new int[n][m];
+        for(int[] row : ans) Arrays.fill(row, -1);
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(mat[i][j]==0){
+                if(mat[i][j] == 0){
                     ans[i][j] = 0;
-                    q.push({i, j});
+                    q.offer(new int[] {i, j});
                 }
             }
         }
-        int dx[4] = {1, -1, 0, 0};
-        int dy[4] = {0, 0, -1, 1};
-        while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+        while(!q.isEmpty()){
+            int[] it = q.poll();
+            int x = it[0], y = it[1];
             for(int k=0; k<4; k++){
                 int r = x+dx[k];
                 int c = y+dy[k];
                 if(r>=0 && r<n && c>=0 && c<m && ans[r][c]==-1){
                     ans[r][c] = ans[x][y]+1;
-                    q.push({r, c});
+                    q.offer(new int[] {r, c});
                 }
             }
-            q.pop();
         }
         return ans;
     }
-};
+}
