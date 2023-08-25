eca84cbd796855b6a31be17550a04110e5963f1b
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-08-25 21:46:41 +0530

    Time: 42 ms (91.84%), Space: 16.5 MB (62.86%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,17 +1,13 @@
-class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1.length()+s2.length() != s3.length()) return false;
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        return f(0, 0, s1, s2, s3, dp);
-    }
-    boolean f(int i, int j, String s1, String s2, String s3, int[][] dp){
-        if(i+j == s3.length()) return true;
-        if(dp[i][j] != -1) return dp[i][j] == 1;;
-        boolean ans = false;
-        if(i<s1.length() && s1.charAt(i)==s3.charAt(i+j)) ans |= f(i+1, j, s1, s2, s3, dp);
-        if(j<s2.length() && s2.charAt(j)==s3.charAt(i+j)) ans |= f(i, j+1, s1, s2, s3, dp);
-        dp[i][j] = ans ? 1 : 0;
-        return ans;
-    }
-}
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1)+len(s2) != len(s3): return False
+        dp = [[-1]*(len(s2)+1) for _ in range(len(s1)+1)]
+        return self.f(0, 0, s1, s2, s3, dp)
+    def f(self, i, j, s1, s2, s3, dp):
+        if i+j == len(s3): return True
+        if dp[i][j] != -1: return dp[i][j]
+        ans = 0
+        if i < len(s1) and s1[i]==s3[i+j]: ans |= self.f(i+1, j, s1, s2, s3, dp)
+        if j < len(s2) and s2[j]==s3[i+j]: ans |= self.f(i, j+1, s1, s2, s3, dp)
+        dp[i][j] = ans
+        return ans
