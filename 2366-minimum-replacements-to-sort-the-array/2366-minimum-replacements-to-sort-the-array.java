81eb6e02a92129278bfe9a018606e73406342136
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-08-30 16:12:48 +0530

    Time: 5 ms (62.50%), Space: 54.8 MB (47.92%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long minimumReplacement(int[] nums) {
+        long ans = 0;
+        int n = nums.length;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] <= nums[i+1]) continue;
+            long numElement = (long)(nums[i]+nums[i+1]-1)/(long)(nums[i+1]);
+            ans += numElement-1;
+            nums[i] = nums[i]/(int)numElement;
+        }
+        return ans;
+    }
+}
