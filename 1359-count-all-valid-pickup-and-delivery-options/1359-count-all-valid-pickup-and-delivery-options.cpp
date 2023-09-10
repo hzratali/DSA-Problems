5ab60e9d505e15eea67f5ea67713fb255f2a6a87
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-09-10 11:27:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.33%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int countOrders(int n) {
+        long places = 2*n, cnt = 1;
+        for(int i=n; i>=1; i--){
+            cnt = (cnt*((places*(places-1))/2))%mod;
+            places -= 2;
+        }
+        return cnt;
+    }
+};
