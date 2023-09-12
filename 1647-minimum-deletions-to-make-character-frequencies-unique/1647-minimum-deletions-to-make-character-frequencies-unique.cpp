b947eef924f01e1e4dc1e40a839b0e735e075080
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-09-12 18:41:27 +0530

    Time: 76 ms (40.42%), Space: 17.4 MB (58.00%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        vector<int> v(26, 0);
-        for(char c : s) v[c-'a']++;
-        sort(v.begin(), v.end(), greater<>());
-        int ans = 0;
-        for(int i=0; i<25; i++){
-            if(v[i]<=v[i+1]){
-                int prev = v[i+1];
-                v[i+1] = max(0, v[i]-1);
-                ans += (prev - v[i+1]);
+        unordered_map<char, int> mp;
+        for(char c : s) mp[c]++;
+        priority_queue<int> pq;
+        for(auto x : mp) pq.push(x.second);
+        int ans=0;
+        while(pq.size() != 1){
+            int top = pq.top(); pq.pop();
+            if(top==pq.top() && top != 0){
+                ans++;
+                pq.push(top-1);
             }
         }
         return ans;
