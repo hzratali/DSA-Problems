e042dc5cf440173314dceecd2d222d49ef2ab265
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-09-14 13:53:36 +0530

    Time: 12 ms (95.76%), Space: 16 MB (18.19%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    map<string, priority_queue<string, vector<string>, greater<string>>> mp;
+    vector<string> ans;
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for(auto x : tickets){
+            mp[x[0]].push(x[1]);
+        }
+        dfs("JFK");
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    void dfs(string s){
+        auto &x = mp[s];
+        while(!x.empty()){
+            string to = x.top(); x.pop();
+            dfs(to);
+        }
+        ans.push_back(s);
+    }
+};
