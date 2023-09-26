28e42d7e6318fcf57e46cf8c01f69134aba557df
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-09-26 19:29:56 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (17.68%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> lastIndex(26, 0);
+        // track the lastIndex of character presence
+        for(int i=0; i<s.size(); i++) lastIndex[s[i]-'a'] = i;
+        vector<bool> seen(26, false); // keep track seen
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            int curr = s[i]-'a';
+            if(seen[curr]) continue; // if seen continue as we need to pick one char only
+            while(st.size()>0 && st.top()>s[i] && i<lastIndex[st.top()-'a']){
+                seen[st.top()-'a'] = false; // pop out and mark unseen
+                st.pop();
+            }
+            st.push(s[i]); // add into stack
+            seen[curr] = true; // mark seen
+        }
+        string ans = "";
+        while(st.size() > 0){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
