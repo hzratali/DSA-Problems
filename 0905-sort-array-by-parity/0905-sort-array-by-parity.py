10a967d498c9de023681b9b10366899eae12be3f
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-09-28 12:32:29 +0530

    Time: 73 ms (76.01%), Space: 17.1 MB (45.53%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,13 +1,8 @@
-class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        int j=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]%2 == 0){
-                int t = nums[i];
-                nums[i] = nums[j];
-                nums[j++] = t;
-            }
-        }
-        return  nums;
-    }
-}
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+        return nums
