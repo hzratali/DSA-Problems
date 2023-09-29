ecf52ba1df001499d9d77e1c34e5cc830cb31cc3
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-09-29 16:59:39 +0530

    Time: 2 ms (62.86%), Space: 55.7 MB (46.47%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,12 +1,11 @@
 class Solution {
-public:
-    bool isMonotonic(vector<int>& nums) {
-        bool increasing=true, decreasing=true;
-        for(int i=1; i<nums.size(); i++){
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true, decreasing = true;
+        for(int i=1; i<nums.length; i++){
             if(nums[i] > nums[i-1]) decreasing = false;
-            if(nums[i] < nums[i-1]) increasing = false;
-            if(!increasing && !decreasing) return false;
+            else if(nums[i] < nums[i-1]) increasing = false;
+            if(increasing==false && decreasing==false) return false;
         }
         return true;
     }
-};
+}
