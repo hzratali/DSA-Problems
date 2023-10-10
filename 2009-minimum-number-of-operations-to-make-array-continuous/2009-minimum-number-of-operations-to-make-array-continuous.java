1a3047f8af97bbefe7a050a82f02cce5eeb8c19c
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-10-10 14:43:18 +0530

    Time: 66 ms (33.33%), Space: 57.6 MB (61.76%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -8,22 +8,12 @@ class Solution {
         int in = 0;
         for(int x : st) arr[in++] = x;
         Arrays.sort(arr);
+        int j = 0;
         for(int i=0; i<arr.length; i++){
-            int l = arr[i];
-            int r = l+n-1;
-            int j = upperBound(arr, r);
+            while(j<arr.length && arr[j] < arr[i]+n) j++;
             int cnt  = j-i;
             ans = Math.min(ans, n-cnt);
         }
         return ans;
-    }
-    int upperBound(int[] arr, int target){
-        int l=0, r = arr.length;
-        while(l<r){
-            int m = (l+r)/2;
-            if(target < arr[m]) r = m;
-            else l = m+1;
-        }
-        return l;
     }
 }
