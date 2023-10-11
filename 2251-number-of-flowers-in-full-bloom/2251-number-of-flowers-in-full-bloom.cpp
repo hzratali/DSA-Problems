af617d3acd89b2bda9ca7689ec802b04e58a719c
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-10-11 17:26:29 +0530

    Time: 250 ms (60.80%), Space: 89.1 MB (45.01%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        vector<int> sortedPeople(people.begin(), people.end());
-        sort(sortedPeople.begin(), sortedPeople.end());
-        sort(flowers.begin(), flowers.end());
-        unordered_map<int, int> mp;
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int i=0;
-        for(int person : sortedPeople){
-            while(i<flowers.size() && flowers[i][0] <= person){
-                pq.push(flowers[i][1]);
-                i++;
-            }
-            while(!pq.empty() && pq.top() < person) pq.pop();
-            mp[person] = pq.size();
+        vector<int> starts, ends;
+        for(auto flower : flowers){
+            starts.push_back(flower[0]);
+            ends.push_back(flower[1]+1);
         }
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
         vector<int> ans;
-        for(int person : people) ans.push_back(mp[person]);
+        for(int person : people){
+            int i = upper_bound(starts.begin(), starts.end(), person) - starts.begin();
+            int j = upper_bound(ends.begin(), ends.end(), person) - ends.begin();
+            ans.push_back(i-j);
+        }
         return ans;
     }
 };
