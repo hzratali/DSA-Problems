a47a7d1d6a8df7346825bb26f63d43d512af5f5f
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-10-29 16:57:02 +0530

    Time: 28 ms (97.60%), Space: 16.2 MB (74.40%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.py
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -1,7 +1,6 @@
-class Solution {
-    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int pigs = 0;
-        while(Math.pow(minutesToTest/minutesToDie + 1, pigs) < buckets) pigs++;
-        return pigs;
-    }
-}
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        pigs = 0
+        while (minutesToTest//minutesToDie+1) ** pigs < buckets:
+            pigs += 1
+        return pigs
