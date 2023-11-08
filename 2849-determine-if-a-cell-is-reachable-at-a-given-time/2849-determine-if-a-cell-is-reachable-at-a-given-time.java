59ed65cc2893c27c6022e4b3d20225262a36d79e
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-11-08 17:05:13 +0530

    Time: 2 ms (36.53%), Space: 6.2 MB (10.74%) - LeetHub

---
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.java
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int width = abs(sx-fx), hight = abs(sy-fy);
+        if(width==0 && hight==0 && t==1) return false;
+        return t>=max(width, hight);
+    }
+};
