d808ad3c86d71b80e4a5d19e2fe1aae8ef4cec7a
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-11-25 12:33:19 +0530

    Time: 727 ms (59.62%), Space: 31.9 MB (30.45%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,20 +1,17 @@
-class Solution {
-    public int[] getSumAbsoluteDifferences(int[] nums) {
-        int n = nums.length, totalSum = 0;
-        for(int x : nums) totalSum += x;
-        int leftSum = 0;
-        int[] ans = new int[n];
-        for(int i=0; i<n; i++){
-            int rightSum = totalSum - leftSum - nums[i];
+class Solution:
+    def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        totalSum = sum(nums)
+        leftSum = 0
+        ans = []
+        for i in range(n):
+            rightSum = totalSum - leftSum - nums[i]
             
-            int rightCnt = n-1-i, leftCnt = i;
+            leftCnt, rightCnt = i, n-1-i
             
-            int leftTotal = leftCnt*nums[i] - leftSum;
-            int rightTotal = rightSum - rightCnt*nums[i];
+            leftTotal = leftCnt * nums[i] - leftSum
+            rightTotal = rightSum - rightCnt * nums[i]
             
-            ans[i] = leftTotal + rightTotal;
-            leftSum += nums[i];
-        }
-        return ans;
-    }
-}
+            ans.append(leftTotal+rightTotal)
+            leftSum += nums[i]
+        return ans
