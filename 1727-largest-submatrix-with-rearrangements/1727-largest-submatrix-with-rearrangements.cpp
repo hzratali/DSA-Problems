2560b6fa25bcb26be986d91a3815559cabbbe90c
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-11-26 11:50:09 +0530

    Time: 1124 ms (27.08%), Space: 42.7 MB (27.08%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,15 +1,11 @@
-class Solution {
-public:
-    int largestSubmatrix(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size(), ans = 0;
-        for(int r=0; r<n; r++){
-            for(int c=0; c<m; c++){
-                if(matrix[r][c] != 0 and r>0) matrix[r][c] += matrix[r-1][c];
-            }
-            vector<int> row = matrix[r];
-            sort(row.begin(), row.end(), greater());
-            for(int i=0; i<m; i++) ans = max(ans,row[i]*(i+1));
-        }
-        return ans;
-    }
-};
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        n, m, ans = len(matrix), len(matrix[0]), 0
+        for r in range(n):
+            for c in range(m):
+                if matrix[r][c]!=0 and r>0:
+                    matrix[r][c] += matrix[r-1][c]
+            row = sorted(matrix[r], reverse=True)
+            for i in range(m):
+                ans = max(ans, row[i]*(i+1))
+        return ans
