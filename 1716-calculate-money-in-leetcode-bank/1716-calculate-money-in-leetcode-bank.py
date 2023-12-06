f87759161ca3ef94c4e1cba87cb49d275f44f07d
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-06 10:02:43 +0530

    Time: 2 ms (12.86%), Space: 39.4 MB (35.00%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,13 @@
+class Solution {
+    public int totalMoney(int n) {
+        int ans = 0, monday = 1;
+        while(n > 0){
+            for(int i=0; i<Math.min(n, 7); i++){
+                ans += monday + i;
+            }
+            n -= 7;
+            monday++;
+        }
+        return ans;
+    }
+}
