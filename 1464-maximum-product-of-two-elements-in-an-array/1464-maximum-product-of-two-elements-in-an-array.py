901ed53cfa83e4010aab5e7a448e72283b0654da
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-12 15:16:14 +0530

    Time: 55 ms (64.56%), Space: 16.3 MB (41.49%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -1,13 +1,9 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        int f=0, s=0;
-        for(int x : nums){
-            if(x >= f){
-                s = f;
-                f = x;
-            }
-            else if(x > s) s = x;
-        }
-        return (f-1)*(s-1);
-    }
-}
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        f, s = 0, 0
+        for x in nums:
+            if x>=f:
+                s = f
+                f = x
+            elif x>s: s = x
+        return (f-1)*(s-1)
