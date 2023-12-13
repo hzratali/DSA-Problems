cb3a64c5d20fe6df74df4383b81ece8fffb41819
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-13 12:00:18 +0530

    Time: 1 ms (99.75%), Space: 43.9 MB (47.47%) - LeetHub

---
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int n=mat.length, m = mat[0].length;
+        int[] rowCnt = new int[n];
+        int[] colCnt = new int[m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1){
+                    rowCnt[i]++;
+                    colCnt[j]++;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1){
+                    if(rowCnt[i]==1 && colCnt[j]==1) ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
