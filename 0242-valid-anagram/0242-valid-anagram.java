b6c7b5440bfcdafc127dcd09fb8d7c13d70be9c3
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-16 12:19:38 +0530

    Time: 8 ms (62.96%), Space: 7.7 MB (45.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s == t) return true;
+        if(s.size() != t.size()) return false;
+        vector<int> v(26, 0);
+        for(int i=0; i<s.size(); i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(v[i]) return false;
+        }
+        return true;
+    }
+};
