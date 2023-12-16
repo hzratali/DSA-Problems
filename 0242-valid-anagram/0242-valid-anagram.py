0f8e87e01d0676917ed7fbdc8ce25f8bf43b3102
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-16 12:33:21 +0530

    Time: 60 ms (26.72%), Space: 16.9 MB (32.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,11 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.equals(t)) return true;
-        if(s.length() != t.length()) return false;
-        int[] v = new int[26];
-        for(int i=0; i<s.length(); i++){
-            v[s.charAt(i)-'a']++;
-            v[t.charAt(i)-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(v[i] != 0) return false;
-        }
-        return true;
-    }
-}
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if s==t: return True
+        if len(s)!=len(t): return False
+        v = [0]*26
+        for i in range(len(s)):
+            v[ord(s[i])-ord('a')] += 1
+            v[ord(t[i])-ord('a')] -= 1
+        for i in range(26):
+            if v[i]: return False
+        return True
