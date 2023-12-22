1a0b3d9ee4cf7587ab10a53e7663173434ff715d
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-22 16:51:48 +0530

    Time: 3 ms (50.89%), Space: 6.8 MB (43.75%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ans = 0, zero = 0, one;
+        int ans = 0;
         for(int i=0; i<s.size()-1; i++){
-            if(s[i] == '0') zero++;
-            one = 0;
-            for(int j=i+1; j<s.size(); j++){
-                if(s[j] == '1'){
-                    one++;
-                }
+            int cnt = 0;
+            for(int j=0; j<=i; j++){
+                if(s[j] == '0') cnt++;
             }
-            ans = max(ans, zero+one);
+            for(int j=i+1; j<s.size(); j++){
+                if(s[j] == '1') cnt++;
+            }
+            ans = max(ans, cnt);
         }
-        return zero==s.size() ? ans-1 : ans;
+        return ans;
     }
 };
