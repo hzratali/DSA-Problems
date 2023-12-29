b7bdd61c566c9bfb7efa0179ce491d5fe7c9735a
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2023-12-30 00:37:08 +0530

    Time: 1667 ms (18.42%), Space: 17.5 MB (60.80%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,20 +1,16 @@
-class Solution {
-public:
-    int dp[301][11];
-    int minDifficulty(vector<int>& jobDifficulty, int d) {
-        int n = jobDifficulty.size();
-        memset(dp, -1, sizeof(dp));
-        if(n < d) return -1;
-        return f(jobDifficulty, n, 0, d);
-    }
-    int f(vector<int> &jobD, int n, int i, int d){
-        if(d == 1) return *max_element(jobD.begin()+i, jobD.end());
-        if(dp[i][d] != -1) return dp[i][d];
-        int ans = INT_MAX, curr = INT_MIN;
-        for(int j=i; j<=n-d; j++){
-            curr = max(curr, jobD[j]);
-            ans = min(ans, curr + f(jobD, n, j+1, d-1));
-        }
-        return dp[i][d] = ans;
-    }
-};
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+        if n < d: return -1
+        dp = [[-1 for _ in range(d + 1)] for _ in range(n + 1)]
+        return self.f(jobDifficulty, n, 0, d, dp)
+    def f(self, jobD, n, i, d, dp):
+        if d == 1:
+            return max(jobD[i:])
+        if dp[i][d] != -1: return dp[i][d]
+        ans, curr = float('inf'), float('-inf')
+        for j in range(i, n-d+1):
+            curr = max(curr, jobD[j])
+            ans = min(ans, curr+self.f(jobD, n, j+1, d-1, dp))
+        dp[i][d] = ans
+        return ans
