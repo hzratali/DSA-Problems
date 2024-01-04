24db39a5de76bd81e716020aa52ad3b85b9fcec1
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2024-01-04 20:37:01 +0530

    Time: 565 ms (90.91%), Space: 31.8 MB (10.30%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,12 +1,8 @@
-class Solution {
-    public int minOperations(int[] nums) {
-        HashMap<Integer, Integer> mp = new HashMap();
-        for(int x : nums) mp.put(x, mp.getOrDefault(x, 0)+1);
-        int ans = 0;
-        for(int cnt : mp.values()){
-            if(cnt == 1) return -1;
-            ans += Math.ceil((double)cnt/3);
-        }
-        return ans;
-    }
-}
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        mp = Counter(nums)
+        ans = 0
+        for cnt in mp.values():
+            if cnt ==1: return -1
+            ans += ceil(cnt/3)
+        return ans
