b1d5471a6601c80d5bd6e56617c54da0eb846d46
Hazrat Ali <89822613+ha-rahman@users.noreply.github.com>
2024-01-16 17:58:49 +0530

    Time: 463 ms (33.05%), Space: 64.5 MB (32.95%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,35 +1,26 @@
-class RandomizedSet {
-public:
-    unordered_set<int> s;
-    RandomizedSet() {
-        
-    }
-    
-    bool insert(int val) {
-        if(s.find(val) != s.end()) return false;
-        else{
-            s.insert(val);
-            return true;
-        }
-    }
-    
-    bool remove(int val) {
-        if(s.find(val) == s.end()) return false;
-        else{
-            s.erase(val);
-            return true;
-        }
-    }
-    
-    int getRandom() {
-        return *next(s.begin(), rand()%s.size());
-    }
-};
+class RandomizedSet:
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
+    def __init__(self):
+        self.s = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.s: return False
+        else:
+            self.s.add(val)
+            return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.s: return False
+        else:
+            self.s.remove(val)
+            return True
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.s))
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
