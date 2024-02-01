f0687448c1e121c2d5dd6ac2ec994c2e71ba8a1e
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-01 11:29:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Panagram Checking/panagram-checking.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to check if a string is Pangram or not.
+    bool checkPangram (string s) {
+        // your code 
+        vector<int> v(26, 0);
+        for(char c : s){
+            if(c >= 'A' && c<= 'Z') v[c-'A']++;
+            if(c >= 'a' && c<= 'z') v[c-'a']++;
+        }
+        for(int x : v){
+            if(x == 0) return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--){
+        string s;
+        getline(cin, s);
+        Solution obj;
+        if (obj.checkPangram(s) == true)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+    return(0);
+}
+
+// } Driver Code Ends
