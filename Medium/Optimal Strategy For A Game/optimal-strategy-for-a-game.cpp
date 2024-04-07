17d7f7d821a59bdb1716187e6dfedfebfab7c815
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-04-08 03:49:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+//Function to find the maximum possible amount of money we can win.
+class Solution{
+    public:
+    long long maximumAmount(int n, int arr[]){
+        // Your code here
+        vector<vector<long long>> dp(n+2, vector<long long>(n+2, -1));
+        return f(0, n-1, arr, dp);
+    }
+    long long f(int i, int n, int arr[], vector<vector<long long>> &dp){
+        if(i > n) return 0;
+        if(dp[i][n] != -1) return dp[i][n];
+        long long ans1 = arr[i] + min(f(i+1, n-1, arr, dp), f(i+2, n, arr, dp));
+        long long ans2 = arr[n] + min(f(i+1, n-1, arr, dp), f(i, n-2, arr, dp));
+        return dp[i][n] = max(ans1, ans2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		cout<< ob.maximumAmount(n,a)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
