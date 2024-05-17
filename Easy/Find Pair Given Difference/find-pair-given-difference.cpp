4eba6d155c348604432dd54942712d7f7d7fe19a
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-05-17 14:39:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Pair Given Difference/find-pair-given-difference.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<n; i++){
+            int find = arr[i]-x;
+            if(binary_search(arr.begin(), arr.end(), find)){
+                if(find == arr[i] && x == 0){
+                    int lb = lower_bound(arr.begin(), arr.end(), find)-arr.begin();
+                    if(lb != i) return 1;
+                }
+                else return 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        Solution obj;
+        int res = obj.findPair(n, x, arr);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
