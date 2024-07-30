2164a1b4cdee1da32cdd78e1aa6790b3fcc43a29
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-07-30 14:39:21 +0530

    Time: 94 ms (54.23%), Space: 23.5 MB (76.55%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.size();
-        vector<int> cntA(n, 0);
-        int aCnt = 0;
-        //First pass: compute count a which store the number of a char to the right of the curr pos
-        for(int i=n-1; i>=0; i--){
-            cntA[i] = aCnt;
-            if(s[i] == 'a') aCnt++;
-        }
-        int mnDel = n, bCnt = 0;
-        //Second Pass: comupte minimum deletions on the fly
+        int n = s.size(), a = 0;
         for(int i=0; i<n; i++){
-            mnDel = min(mnDel, bCnt+cntA[i]);
-            if(s[i] == 'b') bCnt++;
+            if(s[i] == 'a') a++;
+        }
+        int mnDel=n, b =0;
+        for(int i=0; i<n; i++){
+            if(s[i] == 'a') a--;
+            mnDel = min(mnDel, a+b);
+            if(s[i] == 'b') b++;
         }
         return mnDel;
     }
