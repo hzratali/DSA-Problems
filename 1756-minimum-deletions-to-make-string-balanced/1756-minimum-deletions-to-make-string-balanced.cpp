f30ac2156aec97c43fe9a4599f47f35521808fbf
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-07-30 14:36:11 +0530

    Time: 117 ms (21.21%), Space: 38.5 MB (37.32%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        vector<int> cntA(n, 0), cntB(n, 0);
-        int bCnt = 0;
-        //First pass: compute count b which store the number of b char to the left of the curr pos
-        for(int i=0; i<n; i++){
-            cntA[i] = bCnt;
-            if(s[i] == 'b') bCnt++;
-        }
+        vector<int> cntA(n, 0);
         int aCnt = 0;
-        //Second pass: compute count a which store the number of a char to the right of the curr pos
+        //First pass: compute count a which store the number of a char to the right of the curr pos
         for(int i=n-1; i>=0; i--){
-            cntB[i] = aCnt;
+            cntA[i] = aCnt;
             if(s[i] == 'a') aCnt++;
         }
-        int mnDel = n;
-        //Third Pass: Iterate through the string to find the minimum del
-        for(int i=0; i<n; i++) mnDel = min(mnDel, cntA[i]+cntB[i]);
+        int mnDel = n, bCnt = 0;
+        //Second Pass: comupte minimum deletions on the fly
+        for(int i=0; i<n; i++){
+            mnDel = min(mnDel, bCnt+cntA[i]);
+            if(s[i] == 'b') bCnt++;
+        }
         return mnDel;
     }
 };
