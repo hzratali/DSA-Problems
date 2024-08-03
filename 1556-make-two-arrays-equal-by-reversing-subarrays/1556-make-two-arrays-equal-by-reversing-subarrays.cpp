fad3ab0e9440ab5bba21dc3c2f87ea4881e9c081
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-08-03 14:09:18 +0530

    Time: 12 ms (34.04%), Space: 21 MB (6.79%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        sort(target.begin(), target.end());
-        sort(arr.begin(), arr.end());
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i] != target[i]) return false;
+        // Dictionary to maintain frequency count for arr
+        unordered_map<int, int> arrFreq;
+        for (int num : arr) {
+            arrFreq[num]++;
+        }
+        // Dictionary to maintain frequency count for target
+        unordered_map<int, int> targetFreq;
+        for (int num : target) {
+            targetFreq[num]++;
+        }
+        // Number of distinct elements of the 2 arrays are not equal
+        if (arrFreq.size() != targetFreq.size()) {
+            return false;
+        }
+        for (auto it : arrFreq) {
+            // Frequency for num differs
+            if (targetFreq[it.first] != it.second) {
+                return false;
+            }
         }
         return true;
     }
