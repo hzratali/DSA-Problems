db1c59b5fc916f5a4489bf8b3acab5f016fbe761
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-08-18 14:42:12 +0530

    Time: 83 ms (21.76%), Space: 34.2 MB (12.97%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        set<long> uglyNumbers;
-        uglyNumbers.insert(1);
-        long curr = 1;
-        for(int i=0; i<n; i++){
-            curr = *uglyNumbers.begin();  // get the smallest number from the set
-            uglyNumbers.erase(uglyNumbers.begin()); // Remove it from the set
-            //Insert the next potential ugly numbers
-            uglyNumbers.insert(curr*2);
-            uglyNumbers.insert(curr*3);
-            uglyNumbers.insert(curr*5);
+        // Min-heap to store and retrieve the smallest ugly number
+        priority_queue<long, vector<long>, greater<long>> minHeap;
+        unordered_set<long> seenNumbers;  // Set to avoid duplicates
+        vector<int> primeFactors = {
+            2, 3, 5};  // Factors for generating new ugly numbers
+
+        minHeap.push(1);
+        seenNumbers.insert(1);
+
+        long currentUgly = 1;
+        for (int i = 0; i < n; ++i) {
+            currentUgly = minHeap.top();  // Get the smallest number
+            minHeap.pop();                // Remove it from the heap
+
+            // Generate and push the next ugly numbers
+            for (int prime : primeFactors) {
+                long nextUgly = currentUgly * prime;
+                if (seenNumbers.find(nextUgly) ==
+                    seenNumbers.end()) {  // Avoid duplicates
+                    minHeap.push(nextUgly);
+                    seenNumbers.insert(nextUgly);
+                }
+            }
         }
-        return static_cast<int>(curr);   // return the nth ugly number
+
+        return static_cast<int>(currentUgly);  // Return the nth ugly number
     }
 };
