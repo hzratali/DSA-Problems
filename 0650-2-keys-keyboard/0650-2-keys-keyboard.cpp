559f3e1f788feaf859f7bc48eac88f1076dd0b97
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-08-19 15:08:37 +0530

    Time: 8 ms (48.73%), Space: 87.3 MB (19.17%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int minSteps(int n) {
         if(n==1) return 0;
-        return 1 + f(1, 1, n); //First step is always copy all operation
+        vector<vector<int>> dp(n+1, vector<int>(n/2+1, 0));
+        return 1 + f(1, 1, n, dp); //First step is always copy all operation
     }
-    int f(int currLen, int pasteLen, int n){
+    int f(int currLen, int pasteLen, int n, vector<vector<int>>&dp){
         if(currLen == n) return 0; // base case: reached n A's, dont need more operations
         if(currLen > n) return 1000; // base case: exceeded n A's not a valid sequence, so return max value
         // copy all + paste
-        int op1 = 2 + f(currLen*2, currLen, n);
+        if(dp[currLen][pasteLen] != 0) return dp[currLen][pasteLen];
+        int op1 = 2 + f(currLen*2, currLen, n, dp);
         // Paste
-        int op2 = 1 + f(currLen+pasteLen, pasteLen, n);
-        return min(op1, op2);
+        int op2 = 1 + f(currLen+pasteLen, pasteLen, n, dp);
+        dp[currLen][pasteLen] = min(op1, op2);
+        return dp[currLen][pasteLen];
     }
 };
