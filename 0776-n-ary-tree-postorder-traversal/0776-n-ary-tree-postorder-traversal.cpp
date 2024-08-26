239de2b17fcd5d1731583fe5e3f3632d9eeed5cc
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-08-26 15:43:56 +0530

    Time: 9 ms (88.77%), Space: 15.2 MB (49.47%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -1,34 +1,21 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 class Solution {
 public:
     vector<int> postorder(Node* root) {
-        vector<int> ans;
-        if(!root) return ans;
-        f(root, ans);
-        return ans;
-    }
-    void f(Node *currNode, vector<int> &ans){
-        if(!currNode) return;
-        for(Node *child : currNode->children) f(child, ans);
-        ans.push_back(currNode->val);
+        vector<int> result;
+        // If the root is null, return the empty list
+        if (root == nullptr) return result;
+        stack<Node*> nodeStack;
+        nodeStack.push(root);
+        // Traverse the tree using the stack
+        while (!nodeStack.empty()) {
+            Node* currentNode = nodeStack.top();
+            nodeStack.pop();
+            result.push_back(currentNode->val);
+            // Push all the children of the current node to the stack
+            for (Node* child : currentNode->children) nodeStack.push(child);
+        }
+        // Reverse the result list to get the correct postorder traversal
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
