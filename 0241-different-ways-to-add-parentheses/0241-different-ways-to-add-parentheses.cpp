da61296f6c9fbb1870735faf553f537d355aa344
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-09-19 10:42:05 +0530

    Time: 4 ms (46.04%), Space: 9 MB (93.56%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string expression) {
+        vector<vector<vector<int>>> dp(expression.size(), vector<vector<int>>(expression.size()));
+        return computeAns(expression, dp, 0, expression.size()-1);
+    }
+    vector<int> computeAns(string &expression, vector<vector<vector<int>>>&dp, int start, int end){
+        if(!dp[start][end].empty()) return dp[start][end];
         vector<int> ans;
-        if(expression.size() == 0) return ans;
-        if(expression.size() == 1){
-            ans.push_back(stoi(expression));
+        if(start == end){
+            ans.push_back(expression[start]-'0');
             return ans;
         }
-        if(expression.size()==2 && isdigit(expression[0])){
-            ans.push_back(stoi(expression));
+        if(end-start==1 && isdigit(expression[start])){
+            int tens = expression[start]-'0';
+            int ones = expression[end]-'0';
+            ans.push_back(10*tens+ones);
             return ans;
         }
-        for(int i=0; i<expression.size(); i++){
+        for(int i=start; i<=end; i++){
             char currChar = expression[i];
             if(isdigit(currChar)) continue;
-            vector<int> leftAns = diffWaysToCompute(expression.substr(0, i));
-            vector<int> rightAns = diffWaysToCompute(expression.substr(i+1));
+            vector<int> leftAns = computeAns(expression, dp, start, i-1);
+            vector<int> rightAns = computeAns(expression, dp, i+1, end);
             for(int leftValue :leftAns){
                 for(int rightValue : rightAns){
-                    int computeAns = 0;
                     switch(currChar){
                         case '+':
-                            computeAns = leftValue+rightValue;
+                            ans.push_back(leftValue+rightValue);
                             break;
                         case '-':
-                            computeAns = leftValue - rightValue;
+                            ans.push_back(leftValue-rightValue);
                             break;
                         case '*':
-                            computeAns = leftValue * rightValue;
+                            ans.push_back(leftValue*rightValue);
                             break;
                     }
-                    ans.push_back(computeAns);
                 }
             }
         }
+        dp[start][end] = ans;
         return ans;
     }
 };
