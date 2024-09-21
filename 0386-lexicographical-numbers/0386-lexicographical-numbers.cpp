9be03abd2ed996c36a8adbf9bf3d38758c19bc83
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-09-21 14:07:33 +0530

    Time: 8 ms (73.38%), Space: 12.4 MB (93.86%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        vector<int> lexiNumbers;
-        // Start generating numbers from 1 to 9
-        for(int i=1; i<=9; i++) genLexiNums(i, n, lexiNumbers);
-        return lexiNumbers;
-    }
-    void genLexiNums(int currNum, int limit, vector<int> &ans){
-        if(currNum > limit) return;
-        ans.push_back(currNum);
-        for(int nxtDigit=0; nxtDigit<=9; nxtDigit++){
-            int nxtNum = currNum*10 + nxtDigit;
-            //If the next num is within the limit, continue recursion
-            if(nxtNum <= limit) genLexiNums(nxtNum, limit, ans);
-            else break;
+        vector<int> ans;
+        int currNum = 1;
+        for(int i=0; i<n; i++){
+            ans.push_back(currNum);
+            if(currNum*10 <= n) currNum *= 10;
+            else{
+                while(currNum%10 == 9 || currNum >= n) currNum /= 10;
+                currNum++;
+            }
         }
+        return ans;
     }
 };
