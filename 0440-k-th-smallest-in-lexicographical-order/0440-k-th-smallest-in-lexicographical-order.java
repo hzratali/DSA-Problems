cbf991a0f454aa4fec5e5683eb392cc83461dbc2
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-09-22 11:37:39 +0530

    Time: 0 ms (100%), Space: 40.2 MB (58.14%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while(k > 0){
+            int step = cntSteps(n, curr, curr+1);
+            if(step <= k){
+                curr++;
+                k -= step;
+            }
+            else{
+                curr *= 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+    int cntSteps(int n, long prefix1, long prefix2){
+        int step = 0;
+        while(prefix1 <= n){
+            step += Math.min(n+1, prefix2) - prefix1;
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+        return step;
+    }
+}
