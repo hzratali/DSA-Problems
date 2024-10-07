01f96c3d9eaafc1f4ffba3e39f3a0f6428876585
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-07 14:14:58 +0530

    Time: 6 ms (57.32%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int minLength(string s) {
-        while(s.find("AB") != -1 || s.find("CD") != -1){
-            if(s.find("AB") != -1) s.erase(s.find("AB"), 2);
-            else if(s.find("CD") != -1) s.erase(s.find("CD"), 2);
+        stack<char> st;
+        for(char c : s){
+            if(st.empty()){
+                st.push(c);
+                continue;
+            }
+            if(c=='B' && st.top()=='A') st.pop();
+            else if(c=='D' && st.top()=='C') st.pop();
+            else st.push(c);
         }
-        return s.length();
+        return st.size();
     }
 };
