3c96060be51af094c209e76428e83d14d4a02cd8
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-15 16:32:14 +0530

    Time: 31 ms (73.47%), Space: 15.6 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int whitePos = 0;
+        int blackBallCount = 0;
         long long swaps = 0;
         for(int i=0; i<s.size(); i++){
             if(s[i] == '0'){
-                // Calculate the number of swaps needed to move it to the leftmost available position
-                swaps += i - whitePos;
-                // Move the next available position for a white ball one step to the right
-                whitePos++;
+                // Swap with all black balls to its left
+                swaps += (long long) blackBallCount;
+            }
+            else{
+                // Increment the count
+                blackBallCount++;
             }
         }
         return swaps;
