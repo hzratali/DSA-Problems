3fe8193e96d65fb56143b29a4738be910acbdcd2
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-18 23:46:53 +0530

    Time: 120 ms (12.24%), Space: 75.3 MB (15.05%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-        int mxOrVal = 0;
-        for(int x : nums) mxOrVal |= x;
-        vector<vector<int>> dp(nums.size(), vector<int>(mxOrVal+1, -1));
-        return cntSubSets(0, 0, mxOrVal, nums, dp);
-    }
-    int cntSubSets(int i, int currOrVal, int targetOrVal, vector<int> &nums, vector<vector<int>> &dp){
-        // Base case: reached the end of the array
-        if(i == nums.size()) return (currOrVal == targetOrVal) ? 1 : 0;
-        //Check if the result is already memoized
-        if(dp[i][currOrVal] != -1) return dp[i][currOrVal];
-        //Don't include the curr number
-        int notTake = cntSubSets(i+1, currOrVal, targetOrVal, nums, dp);
-        //Include the curr number
-        int take = cntSubSets(i+1, currOrVal|nums[i], targetOrVal, nums, dp);
-        // return the sum of both cases
-        return dp[i][currOrVal] = take+notTake;
+        vector<int> dp(1<<17, 0);
+        dp[0] = 1;
+        int mx = 0;
+        for(int x : nums){
+            for(int i=mx; i>=0; i--) dp[i | x] += dp[i];
+            mx |= x;
+        }
+        return dp[mx];
     }
 };
