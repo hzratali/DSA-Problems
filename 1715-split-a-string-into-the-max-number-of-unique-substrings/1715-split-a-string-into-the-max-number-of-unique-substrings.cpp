64c2b86da16d3b3100dfaf98421630cd202d6bc4
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-21 13:29:24 +0530

    Time: 11 ms (100%), Space: 12.8 MB (95.32%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     int maxUniqueSplit(string s) {
         unordered_set<string> seen;
-        return backtrack(0, s, seen);
-    }
-    int backtrack(int i, string &s, unordered_set<string> &seen){
-        if(i == s.size()) return 0;
         int mxCnt = 0;
+        backtrack(0, 0, s, seen, mxCnt);
+        return mxCnt;
+    }
+    void backtrack(int i, int cnt, string &s, unordered_set<string> &seen, int &mxCnt){
+        if(cnt + (s.size()-i) <= mxCnt) return;
+        if(i == s.size()){
+            mxCnt = max(mxCnt, cnt);
+            return;
+        }
         for(int j=i+1; j<=s.size(); j++){
             string subS = s.substr(i, j-i);
             if(seen.find(subS) == seen.end()){
                 seen.insert(subS);
-                mxCnt = max(mxCnt, 1+backtrack(j, s, seen));
+                backtrack(j, cnt+1, s, seen, mxCnt);
                 seen.erase(subS);
             }
         }
-        return mxCnt;
     }
 };
