1a76c2b63789f047389c5d80571cd6c9db77a74a
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-22 14:18:22 +0530

    Time: 20 ms (97.75%), Space: 156 MB (5.08%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        priority_queue<long> pq;
+        priority_queue<long, vector<long>, greater<long> > pq;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
@@ -24,9 +24,9 @@ public:
                 if(node->right) q.push(node->right);
             }
             pq.push(sum);
+            if(pq.size() > k) pq.pop();
         }
         if(pq.size() < k) return -1;
-        for(int i=0; i<k-1; i++) pq.pop();
         return pq.top();
     }
 };
