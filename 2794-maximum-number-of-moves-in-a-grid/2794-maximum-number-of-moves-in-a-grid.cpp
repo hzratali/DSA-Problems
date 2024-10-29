1b64c8240e8a8f93238442f5487761de1ad97a5b
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-29 15:31:39 +0530

    Time: 20 ms (54.35%), Space: 74.5 MB (43.37%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int maxMoves(vector<vector<int>>& grid) {
+    // The three possible directions for the next column.
+    const int dirs[3] = {-1, 0, 1};
+    int DFS(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp) {
         int n = grid.size(), m = grid[0].size();
-        queue<vector<int>> q;
-        vector<vector<int>> vis(n, vector<int> (m, 0));
-        for(int i=0; i<n; i++){
-            vis[i][0] = 1;
-            q.push({i, 0, 0});
-        }
-        int dirs[3] = {-1, 0, 1};
-        int mxMoves = 0;
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                vector<int> v = q.front(); q.pop();
-                int r = v[0], c = v[1], cnt = v[2];
-                mxMoves = max(mxMoves, cnt);
-                for(int d : dirs){
-                    int nRow = r+d, nCol = c+1;
-                    if(nRow >= 0 && nCol >= 0 && nRow < n && nCol < m && !vis[nRow][nCol] && grid[r][c]<grid[nRow][nCol]){
-                        vis[nRow][nCol] = 1;
-                        q.push({nRow, nCol, cnt+1});
-                    }
-                }
+        // If we have calculated the moves required for this cell, return the
+        // answer and skip the recursion.
+        if (dp[row][col] != -1) return dp[row][col];
+        int maxMoves = 0;
+        for (int dir : dirs) {
+            // Next cell after the move.
+            int newRow = row + dir, newCol = col + 1;
+            // If the next cell is valid and greater than the current cell
+            // value, perform recursion to that cell with updated value of
+            // moves.
+            if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m &&
+                grid[row][col] < grid[newRow][newCol]) {
+                maxMoves = max(maxMoves, 1 + DFS(newRow, newCol, grid, dp));
             }
         }
-        return mxMoves;
+        return dp[row][col] = maxMoves;
+    }
+    int maxMoves(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int maxMoves = 0;
+        for (int i = 0; i < n; i++) {
+            int movesRequired = DFS(i, 0, grid, dp);
+            maxMoves = max(maxMoves, movesRequired);
+        }
+        return maxMoves;
     }
 };
