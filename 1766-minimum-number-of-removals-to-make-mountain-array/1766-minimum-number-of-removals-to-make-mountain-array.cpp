4c68ab3948bd7e6b484e9c81ed7c57d248742b1d
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-10-31 00:25:18 +0530

    Time: 3 ms (90.91%), Space: 15.2 MB (24.81%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
+    vector<int> getLongestIncreasingSubsequenceLength(vector<int>& v) {
+        vector<int> lisLen(v.size(), 1);
+        vector<int> lis = {v[0]};
+
+        for (int i = 1; i < v.size(); i++) {
+            // Index of the first element which is equal to or greater than
+            // v[i].
+            int index = lower_bound(lis.begin(), lis.end(), v[i]) - lis.begin();
+
+            // Add to the list if v[i] is greater than the last element.
+            if (index == lis.size()) {
+                lis.push_back(v[i]);
+            } else {
+                // Assign the value v[i] to the returned index as it's going to
+                // smaller than or equal to the existing element.
+                lis[index] = v[i];
+            }
+
+            lisLen[i] = lis.size();
+        }
+
+        return lisLen;
+    }
     int minimumMountainRemovals(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> lisLen(n, 1);
-        vector<int> ldsLen(n, 1);
-        // Stores the length of longest increasing subsequence that ends at i.
-        for(int i=0; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                if(nums[i] > nums[j]) lisLen[i] = max(lisLen[i], lisLen[j]+1);
+        int N = nums.size();
+        vector<int> lisLength = getLongestIncreasingSubsequenceLength(nums);
+
+        reverse(nums.begin(), nums.end());
+        vector<int> ldsLength = getLongestIncreasingSubsequenceLength(nums);
+        // Reverse the length array to correctly depict the lenght of longest
+        // decreasing subsequnec for each index.
+        reverse(ldsLength.begin(), ldsLength.end());
+
+        int minRemovals = INT_MAX;
+        for (int i = 0; i < N; i++) {
+            if (lisLength[i] > 1 && ldsLength[i] > 1) {
+                minRemovals =
+                    min(minRemovals, N - lisLength[i] - ldsLength[i] + 1);
             }
         }
-        // Stores the length of longest decreasing subsequence that starts at i.
-        for(int i=n-1; i>=0; i--){
-            for(int j=i+1; j<n; j++){
-                if(nums[i] > nums[j]) ldsLen[i] = max(ldsLen[i], ldsLen[j]+1);
-            }
-        }
-        int mnRemovals = INT_MAX;
-        for(int i=0; i<n; i++){
-            if(lisLen[i] > 1 && ldsLen[i] > 1){
-                mnRemovals = min(mnRemovals, n-lisLen[i]-ldsLen[i]+1);
-            }
-        }
-        return mnRemovals;
+
+        return minRemovals;
     }
 };
