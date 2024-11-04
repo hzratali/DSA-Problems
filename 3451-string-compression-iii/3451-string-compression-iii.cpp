0db6bf4c6e47f2f2eb1a192b2c0576a679326aa1
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-11-04 14:56:31 +0530

    Time: 59 ms (21.37%), Space: 29.9 MB (24.06%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     string compressedString(string word) {
         string comp = "";
-        int cnt = 1;
-        for(int i=1; i<word.size(); i++){
-            if(word[i] == word[i-1]){
+        int i = 0;
+        while(i < word.size()){
+            int cnt = 0;
+            char currChar = word[i];
+            while(i < word.size() && cnt < 9 && word[i] == currChar){
                 cnt++;
-                if(cnt == 10){
-                    comp += "9" + string(1, word[i-1]);
-                    cnt = 1;
-                }
+                i++;
             }
-            else{
-                comp += to_string(cnt) + word[i-1];
-                cnt = 1;
-            }
+            comp += to_string(cnt) + currChar;
         }
-        comp += to_string(cnt)+word[word.size()-1];
         return comp;
     }
 };
