7cd5f6911de3cef0a61f9fbe02c204b235297a5b
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-11-06 16:27:09 +0530

    Time: 0 ms (100%), Space: 31.3 MB (63.03%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n-i-1; j++){
-                if(nums[j] <= nums[j+1]) continue;
-                else{
-                    if(__builtin_popcount(nums[j]) == __builtin_popcount(nums[j+1])){
-                        swap(nums[j], nums[j+1]);
-                    }
-                    else return false;
+        // Number of set bits of the elements in the current segment
+        int numOfSetBits = __builtin_popcount(nums[0]);
+        int maxOfSegment = nums[0];
+        int minOfSegment = nums[0];
+        // Initialize max of the previous segment to the smallest possible
+        // integer
+        int maxOfPrevSegment = INT_MIN;
+        for (int i = 1; i < nums.size(); i++) {
+            if (__builtin_popcount(nums[i]) == numOfSetBits) {
+                // Element belongs to the same segment
+                // Update min and max values of the segment
+                maxOfSegment = max(maxOfSegment, nums[i]);
+                minOfSegment = min(minOfSegment, nums[i]);
+            } else {  // Element belongs to a new segment
+                // Check if the segments are arranged properly
+                if (minOfSegment < maxOfPrevSegment) {
+                    return false;
                 }
+
+                // Update the previous segment's max
+                maxOfPrevSegment = maxOfSegment;
+
+                // Start a new segment with the current element
+                maxOfSegment = nums[i];
+                minOfSegment = nums[i];
+                numOfSetBits = __builtin_popcount(nums[i]);
             }
+        }
+        // Final check for proper segment arrangement
+        if (minOfSegment < maxOfPrevSegment) {
+            return false;
         }
         return true;
     }
