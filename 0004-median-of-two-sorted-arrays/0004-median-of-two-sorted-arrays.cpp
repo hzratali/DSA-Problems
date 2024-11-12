7ff76523988896a132cc377ea3ebe9e973b50b29
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-11-12 16:47:55 +0530

    Time: 4 ms (26.13%), Space: 95.3 MB (42.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    int p1=0, p2=0;
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(), m=nums2.size();
-        if((m+n)%2 == 0){
-            for(int i = 0; i<(m + n)/2-1; ++i){
-                int _ = getMin(nums1, nums2);
-            }
-            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
+        // vector<int> arr;
+        // for(int x : nums1) arr.push_back(x);
+        // for(int x : nums2) arr.push_back(x);
+        // sort(arr.begin(), arr.end());
+        // if(arr.size()%2) return (double) arr[arr.size()/2];
+        // else return (double)(arr[arr.size()/2]+arr[arr.size()/2 - 1])/2;
+        int n = nums1.size(), m = nums2.size();
+        vector<int> arr(n+m, 0);
+        int i = 0, j = 0, k = -1;
+        while(i < n && j < m){
+            if(nums1[i] <= nums2[j]) arr[++k] = nums1[i++];
+            else arr[++k] = nums2[j++];
         }
-        else{
-            for(int i=0; i<(m + n)/2; ++i) {
-                int _ = getMin(nums1, nums2);
-            }
-            return getMin(nums1, nums2);
-        }
-        return -1;
-    }
-    int getMin(vector<int>&nums1, vector<int>&nums2){
-        if(p1<nums1.size() && p2<nums2.size()){
-            return nums1[p1]<nums2[p2] ? nums1[p1++] : nums2[p2++];
-        }
-        else if(p1<nums1.size()){
-            return nums1[p1++];
-        }
-        else if(p2<nums2.size()){
-            return nums2[p2++];
-        }
-        return -1;
+        while(i < n) arr[++k] = nums1[i++];
+        while(j < m) arr[++k] = nums2[j++];
+        if(arr.size()%2) return (double) arr[arr.size()/2];
+        else return (double)(arr[arr.size()/2]+arr[arr.size()/2 - 1])/2;
     }
 };
