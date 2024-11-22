108bff4f02e35e8af0b213d49bd8da78fbbe03d2
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-11-22 16:19:08 +0530

    Time: 31 ms (55.07%), Space: 82.5 MB (21.64%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        int m = matrix[0].size(), mxIdenticalRows = 0;
+        unordered_map<string, int> mp;
         for(auto row : matrix){
-            vector<int> flipRow(m);
-            int identicalRowCnt = 0;
-            for(int j=0; j<m; j++) flipRow[j] = 1 - row[j];
-            for(auto compRow : matrix){
-                if(compRow == row || compRow == flipRow) identicalRowCnt++;
+            string pattern = "";
+            for(int j=0; j<row.size(); j++){
+                if(row[0] == row[j]) pattern += "T";
+                else pattern += "F";
             }
-            mxIdenticalRows = max(mxIdenticalRows, identicalRowCnt);
+            mp[pattern]++;
         }
-        return mxIdenticalRows;
+        int mxFreq = 0;;
+        for(auto &entry : mp) mxFreq = max(entry.second, mxFreq);
+        return mxFreq;
     }
 };
