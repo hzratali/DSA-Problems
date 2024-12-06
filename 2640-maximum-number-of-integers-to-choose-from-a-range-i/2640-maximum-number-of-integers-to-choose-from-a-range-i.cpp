131633ad81b9b914d678e374f15177dce6c11df3
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-12-06 23:05:09 +0530

    Time: 64 ms (82.82%), Space: 117.5 MB (90.75%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        int cnt = 0, sum = 0;
-        set<int> bannedSet(banned.begin(), banned.end());
+        sort(banned.begin(), banned.end());
+        int cnt = 0;
         for(int i=1; i<=n; i++){
-            if(bannedSet.find(i) == bannedSet.end()){
-                sum += i;
-                if(sum <= maxSum) cnt++;
-            }
+            if(binarySearch(i, banned)) continue;
+            maxSum -= i;
+            if(maxSum < 0) break;
+            cnt++;
         }
         return cnt;
+    }
+    bool binarySearch(int target, vector<int> &arr){
+        int l = 0, r = arr.size()-1;
+        while(l <= r){
+            int m = (l+r) / 2;
+            if(arr[m] == target) return true;
+            if(arr[m] > target) r = m-1;
+            else l = m+1;
+        }
+        return false;
     }
 };
