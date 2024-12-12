bc1d500ec082ae3e115db4b1519160fae87db9ce
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-12-12 23:35:34 +0530

    Time: 1 ms (68.38%), Space: 13.3 MB (16.26%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        int n = gifts.size();
+        priority_queue<int> pq(gifts.begin(), gifts.end());
         for(int i=0; i<k; i++){
-            int richPileInd  = 0;
-            for(int currInd=0; currInd<n; currInd++){
-                if(gifts[richPileInd] < gifts[currInd]) richPileInd = currInd;
-            }
-            gifts[richPileInd] = sqrt(gifts[richPileInd]);
+            int ele = pq.top(); pq.pop();
+            pq.push(sqrt(ele));
         }
-        long long int ans = accumulate(gifts.begin(), gifts.end(), 0LL);
+        long long ans = 0;
+        while(!pq.empty()){
+            int ele = pq.top(); pq.pop();
+            ans += ele;
+        }
         return ans;
     }
 };
