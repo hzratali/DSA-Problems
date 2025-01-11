aa62e00a8e7d8de73f64436e3670d2bad3d2e31a
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2025-01-11 11:10:31 +0530

    Time: 4 ms (94.6%), Space: 45.4 MB (61.15%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(s.length() < k) return false;
+        if(s.length() == k) return true;
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()) freq[c-'a']++;
+        int oddCnt = 0;
+        for(int i=0; i<26; i++){
+            if(freq[i]%2==1) oddCnt++;
+        }
+        return oddCnt<=k;
+    }
+}
