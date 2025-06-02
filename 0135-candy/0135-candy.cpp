9949449f281a8da2f67d6506ba6703ceef01310b
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2025-06-02 23:56:38 +0530

    Time: 1 ms (36.28%), Space: 23.3 MB (22.48%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,29 +2,16 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int candy = n, i = 1;
-        while(i < n){
-            if(ratings[i] == ratings[i-1]){
-                i++;
-                continue;
-            }
-            //for increasing slope
-            int peek = 0;
-            while(ratings[i] > ratings[i-1]){
-                peek++;
-                candy += peek;
-                i++;
-                if(i == n) return candy;
-            }
-            //for decreasing slope
-            int valley = 0;
-            while(i<n && ratings[i] < ratings[i-1]){
-                valley++;
-                candy += valley;
-                i++;
-            }
-            candy -= min(peek, valley);
+        vector<int> left(n, 1);
+        vector<int> right(n, 1);
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]) left[i] = left[i-1] + 1;
         }
-        return candy;
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]) right[i] = right[i+1] +1;
+        }
+        int candies = 0;
+        for(int i=0; i<n; i++) candies += max(left[i], right[i]);
+        return candies;
     }
 };
